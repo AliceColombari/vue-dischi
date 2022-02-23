dcbe58ee57d0fa66ff47ec2294dbdfdb1140c06b
AliceColombari <colombari.23@gmail.com>
2022-02-23 13:28:08 +0100

    base generale

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <MyHeader/>
+  </div>
+</template>
+
+<script>
+import MyHeader from './components/MyHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyHeader,
+  }
+}
+</script>
+
+<style lang="scss">
+@import './style/general.scss';
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    PROVA
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
